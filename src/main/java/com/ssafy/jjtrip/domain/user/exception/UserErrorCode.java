8f894dff014c0d8333805297d13ee7422d6f3ba9
zhy2on <52701529+zhy2on@users.noreply.github.com>
2025-11-27 19:05:03 +0900

    feat: USER_NOT_FOUND 에러코드 추가

--- src/main/java/com/ssafy/jjtrip/domain/user/exception/UserErrorCode.java
+++ src/main/java/com/ssafy/jjtrip/domain/user/exception/UserErrorCode.java
@@ -10,7 +10,8 @@ import org.springframework.http.HttpStatus;
 public enum UserErrorCode implements ErrorCode {
 
     INVALID_ROLE_ID("USER_001", "DB에 유효하지 않은 Role ID가 존재합니다.", HttpStatus.INTERNAL_SERVER_ERROR),
-    INVALID_USER_STATUS_ID("USER_002", "DB에 유효하지 않은 User Status ID가 존재합니다.", HttpStatus.INTERNAL_SERVER_ERROR)
+    INVALID_USER_STATUS_ID("USER_002", "DB에 유효하지 않은 User Status ID가 존재합니다.", HttpStatus.INTERNAL_SERVER_ERROR),
+    USER_NOT_FOUND("USER_003", "해당하는 사용자를 찾을 수 없습니다.", HttpStatus.NOT_FOUND)
     ;
 
     private final String code;
