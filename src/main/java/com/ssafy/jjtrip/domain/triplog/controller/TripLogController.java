9f9cb18d91c0bfc2728dc3a82016736ea2f4695b
zhy2on <dhwlgus5940@gmail.com>
2025-12-18 14:38:15 +0900

    refactor: 부분 변경이니 PATCH로 변경

--- src/main/java/com/ssafy/jjtrip/domain/triplog/controller/TripLogController.java
+++ src/main/java/com/ssafy/jjtrip/domain/triplog/controller/TripLogController.java
@@ -21,9 +21,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -81,7 +81,7 @@ public class TripLogController {
         return ResponseEntity.ok(tripLogService.getTripLogDetail(logId));
     }
 
-    @PutMapping("/{logId}")
+    @PatchMapping("/{logId}")
     public ResponseEntity<?> updateTripLog(
             @PathVariable Long logId,
             @Valid @RequestBody TripLogUpdateRequestDto requestDto,
