5e262e4f402a62fdf38a44c868e36d06984e613a
zhy2on <dhwlgus5940@gmail.com>
2025-12-20 02:05:58 +0900

    feat: 특수문자 처리 추가

--- src/main/java/com/ssafy/jjtrip/domain/search/service/TripLogSearchService.java
+++ src/main/java/com/ssafy/jjtrip/domain/search/service/TripLogSearchService.java
@@ -57,6 +57,27 @@ public class TripLogSearchService {
     }
 
     private static String escapeJson(String s) {
-        return s == null ? "" : s.replace("\\", "\\\\").replace("\"", "\\\"");
+        if (s == null) return "";
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            switch (c) {
+                case '"' -> sb.append("\\\"");
+                case '\\' -> sb.append("\\\\");
+                case '\b' -> sb.append("\\b");
+                case '\f' -> sb.append("\\f");
+                case '\n' -> sb.append("\\n");
+                case '\r' -> sb.append("\\r");
+                case '\t' -> sb.append("\\t");
+                default -> {
+                    if (c < ' ') {
+                        sb.append(String.format("\\u%04x", (int) c));
+                    } else {
+                        sb.append(c);
+                    }
+                }
+            }
+        }
+        return sb.toString();
     }
 }
