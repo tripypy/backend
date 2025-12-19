331d2e63e0c7e2a71f5955d9c1579d355dce7bdd
최진우 <jk62362@mensakorea.org>
2025-12-19 14:12:24 +0900

    fix: FriendController API 경로 수정

--- src/main/java/com/ssafy/jjtrip/domain/friend/controller/FriendController.java
+++ src/main/java/com/ssafy/jjtrip/domain/friend/controller/FriendController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/friends")
+@RequestMapping("/friends")
 @RequiredArgsConstructor
 public class FriendController {
 
